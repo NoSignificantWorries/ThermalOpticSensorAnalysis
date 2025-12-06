adf591a4a4d15ac4b0a30b176e844452ce37a289
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2025-12-07 00:52:18 +0700

    Start of working with clickhouse and grafana
    Created basic tables for data from thermal sensors and compose file to
    launch dashboard and storage.

--- dmitry/db/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS sensor_data (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    sensor_line_id INTEGER NOT NULL,
-    timestamp TIMESTAMP NOT NULL,
-    spatial_metadata TEXT,  -- JSON хранится как TEXT
-    value REAL  -- вместо DOUBLE PRECISION
-);
-
-CREATE INDEX IF NOT EXISTS idx_sensor_data_timestamp ON sensor_data(timestamp);
---
+++ storage/init.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE IF NOT EXISTS sensors_db
+ENGINE = Atomic COMMENT 'Database for sensors data';
+
+USE sensors_db;
+
+CREATE TABLE IF NOT EXISTS config0
+(
+  sensor_id UInt32,
+  label Nullable(String),
+  description Nullable(string),
+  created_at DateTime DEFAULT now(),
+)
+ENGINE = MergeTree
+ORDER BY (sensor_id)
+COMMENT 'Config #0 created as default one';
+
+CREATE TABLE IF NOT EXISTS group0
+(
+  sensor_id UInt32,
+  group_id UInt32,
+  start Float32,
+  end Float32,
+)
+ENGINE = MergeTree
+ORDER BY (sensor_id, group_id)
+COMMENT 'Groups table #0 paired with config0 table';
+
+CREATE TABLE IF NOT EXISTS segment0
+(
+  sensor_id UInt32,
+  group_id UInt32,
+  segment_id UInt32,
+  start Float32,
+  end Float32,
+)
+ENGINE = MergeTree
+ORDER BY (sensor_id, group_id, segment_id)
+COMMENT 'Segments table #0 paired with group0 table';
+
+CREATE TABLE IF NOT EXISTS measurements
+(
+  timestamp DateTime64(3, 'UTC'),
+  sensor_id UInt32,
+  group_id UInt32,
+  distance Float32,
+  temp Float32,
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(timestamp)
+ORDER BY (timestamp, group_id, distance)
+SETTINGS index_granularity = 8192
+COMMENT 'Main table of measurements';
+
